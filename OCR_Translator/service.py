72c7a9d6fc47106505d6f3a0d19b7de803127927
Jipsa <jipsaplath@gmail.com>
2018-02-27 15:00:53 -0600

    JSON response containing extracted words from the image

---
+++ OCR_Translator/service.py
@@ -0,0 +1,20 @@
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'C:\Users\Jipsa\Documents\UTTyler-Spring 2018\COSC 5399\api_key.json'
+
+vision_client = vision.ImageAnnotatorClient()
+
+with io.open('./123.jpg', 'rb') as image_file:
+    content = image_file.read()
+
+    image = types.Image(content=content)
+
+response = vision_client.text_detection(image=image)
+texts = response.text_annotations
+print('Texts:')
+
+for text in texts:
+        print('\n"{}"'.format(text.description))
