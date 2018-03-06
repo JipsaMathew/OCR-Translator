6d572a5ed3bb4fbbd6a798e377757285bafe3549
Jipsa <jipsaplath@gmail.com>
2018-03-06 16:54:12 -0600

    added main() to access service.py

--- OCR_Translator/views.py
+++ OCR_Translator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from . import service
+
+from OCR_Translator.service import detect_text, translate_text
+
 
 # Create your views here.
 
@@ -7,5 +9,17 @@ def index(request):
     return render(request, 'OCR_Translator/index.html')
 
 
+def main():
+    """main method for service class"""
+    path = r'C:\Users\Jipsa\Documents\UTTyler-Spring 2018\COSC 5399\cosc5399\OCR_Translator\atlanta.jpg'
+    msg = detect_text(path)  # detected text from the image
+    print(msg)
+
+    lang = 'de'
+    t_msg = translate_text(msg, lang)
+    print(t_msg)
 
 
+# calling main method
+
+main()
