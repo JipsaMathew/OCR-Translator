3fe40bb49cef85df50db6d6090d6571e1fadc713
Jipsa <jipsaplath@gmail.com>
2018-03-06 15:34:20 -0600

    Updated service.py

--- OCR_Translator/static/OCR_Translator/index_js.js
+++ OCR_Translator/static/OCR_Translator/index_js.js
@@ -1,7 +1,8 @@
      function readURL(input) {
+        var url = input.value;
+        //alert(input.value);
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
-
                 reader.onload = function (e) {
                     $('#ocr')
                         .attr('src', e.target.result)
@@ -10,5 +11,6 @@
                 };
 
                 reader.readAsDataURL(input.files[0]);
+
             }
         }
--- OCR_Translator/views.py
+++ OCR_Translator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from . import service
 
 # Create your views here.
+
 def index(request):
     return render(request, 'OCR_Translator/index.html')
+
+
+
+
